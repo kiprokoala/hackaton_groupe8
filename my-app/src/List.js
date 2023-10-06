e2b2088a49705cd976548b1cb470aa30dfec7184
Romain <82060667+kiprokoala@users.noreply.github.com>
2023-10-06 12:17:01 +0200

    correctifs front

--- my-app/src/List.js
+++ my-app/src/List.js
@@ -10,11 +10,49 @@ class List extends Component {
         post: {}
     }
 
+    data = [
+        {user: "Marcel Dupond", com: "Je pense que cette exposition est un très bon choix ! Ma femme et moi ne sommes pas fan de la culture, mais nous avons appris beaucoup de choses durant ce voyage. Nous ne connaissions pas la ville de Paris, mais le lieu a été très facile à trouver."},
+        {user: "Antoine Garcia", com: "Cette expérience enrichissante m'a permis de découvrir de nouveaux horizons. Mon voyage à Paris m'a fait découvrir de nombreuses choses, et je remercie ce site d'avoir regroupé les possibilités."},
+        {user: "ano", com: "Cette exposition est honteuse ! À ne pas faire !!!"},
+        {user: "Jacques Nolti", com: "A refaire sans aucun doute."}
+    ]
+
+    add_com(e){
+        e.preventDefault()
+        var login = sessionStorage.getItem("login")
+        var text = document.getElementById("input_com").value
+        if(login == "null" || login == null || login == ""){
+            console.log("ano added a com " + text);
+        }else{
+            console.log(login + " added a com " + text);
+        }
+    }
+
+    toggleDetail(id) {
+        var temp = this.state.showDetail
+        this.setState({
+            showDetail: !this.state.showDetail
+        });
+        if(temp == false){
+            fetch('https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/records?where=id:' + id).then((response) =>{
+                return response.json()
+            }).then((result) => {
+                this.setState({post: result, showDetail: true})
+            })
+        }else{
+        fetch('https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/records?limit=100&order_by=date_end&where=tags:\'' + this.props.params.id + '\'').then((response) =>{
+            return response.json()
+        }).then((result) => {
+            this.setState({post: result, showDetail: false})
+        })
+        }
+    }
+
     componentDidMount(){
         fetch('https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/que-faire-a-paris-/records?limit=100&order_by=date_end&where=tags:\'' + this.props.params.id + '\'').then((response) =>{
             return response.json()
         }).then((result) => {
-            this.setState({post: result})
+            this.setState({post: result, showDetail: false})
         })
     }
 
@@ -25,14 +63,48 @@ class List extends Component {
         <div className="total">
             <Left_menu />
             <div className="content">
-                <div className="actus">
-                    {this.state.post.results?.map(actu =>
-                        <div className="actualite" id={actu.title}>
-                            <div className="title">{`${actu.title}`}</div>
-                            <img className="actu_image" src={actu.cover_url}/>
+                {this.state.showDetail ?
+                    this.state.post.results?.map(actu =>
+                        <div className="detail" id={actu.title}>
+                            <div className="close_detail" onClick={this.toggleDetail.bind(this, 0)}>X</div>
+                            <h2>{`${actu.title}`}</h2>
+                            <div className="infos_detail">
+                                <img className="detail_image" src={actu.cover_url}/>
+                                <div>
+                                    <h3>Date : {`${actu.date_start}`} au {`${actu.date_end}`}</h3>
+                                    <h3>Lieu : {`${actu.address_street}`} {`${actu.address_zipcode}`}</h3>
+                                    <h3>{`${actu.description}`}</h3>
+                                    <a target="_blank" className="detail_link" href={`${actu.url}`}>Lien de l'évènement</a>
+                                </div>
+                            </div>
+                            <div className="comments">
+                                {this.data.map(com =>
+                                    <div className="comment">
+                                        {`${com.user}`}
+                                        <div className="comment_text">
+                                            {`${com.com}`}
+                                        </div>
+                                    </div>
+                                )}
+                                <div className="comment_form">
+                                    <textarea className="comment_input" id="input_com" />
+                                    <div className="comment_add" onClick={this.add_com.bind(this)}>
+                                        Ajouter commentaire
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    )}
-                </div>
+                    )
+                    :
+                    <div className="actus">
+                        {this.state.post.results?.map(actu =>
+                            <div className="actualite" onClick={this.toggleDetail.bind(this, actu.id)} id={actu.title}>
+                                <div className="title">{`${actu.title}`}</div>
+                                <img className="actu_image" src={actu.cover_url}/>
+                            </div>
+                        )}
+                    </div>
+                }
             </div>
         </div>
     </div>
--- my-app/src/components/Question.js
+++ my-app/src/components/Question.js
@@ -4,29 +4,38 @@ import './Question.css';
 import React from 'react';
 
 function Question() {
-  const data = [
-        {title: "Aimez-vous les pâtes ?", options: ["Oui", "Non", "Ne sais pas", "Sans opinion"]},
-        {title:"Par ici", options: []},
-        {title:"Mais non", options: []},
-        {title:"ouh", options: []}
-  ]
-  return (
-    <div className="questions">
-        {data.map(question =>
-            <div className="question" id={question.title}>
-                {`${question.title}`}
-                <fieldset>
-                    {question.options.map((option) =>
-                        <div className="option">
-                            <input type="radio" id={option} name="answer" value={option} />
-                            <label htmlFor="option">{option}</label>
-                        </div>
-                    )}
-                </fieldset>
+    const data = [
+        {title: "Aimez-vous le théâtre ?", options: ["Oui", "Non", "Ne sais pas", "Sans opinion"]},
+        {title:"Et si nos coeurs débordent vous intéresse ?", options: ["Oui", "Non", "Ne peut pas le voir", "Oui, mais trop cher"]},
+        {title:"Le recommanderiez-vous ?", options: ["Oui, au cercle proche", "Oui, à tous", "Pas encore vu", "Non"]},
+        {title:"Où l'avez-vous vu ?", options: ["Réseaux sociaux", "Affiches physiques", "Recommandation", "Par hasard"]}
+    ]
+
+    const save=(e) =>{
+        e.preventDefault()
+        console.log("answer saved")
+    }
+
+    return (
+        <div className="questions">
+            {data.map(question =>
+                <div className="question" id={question.title}>
+                    {`${question.title}`}
+                    <fieldset>
+                        {question.options.map((option) =>
+                            <div className="option">
+                                <input type="radio" id={option} name="answer" value={option} />
+                                <label htmlFor="option">{option}</label>
+                            </div>
+                        )}
+                    </fieldset>
+                </div>
+            )}
+            <div className="button_save" onClick={save.bind(this)}>
+                coucou
             </div>
-        )}
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Question;
